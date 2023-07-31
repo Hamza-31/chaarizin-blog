179038428b6dde4702fe96544221d8abb92d830d
Hamza <hamza.labzioui@gmail.com>
2023-07-31 19:48:27 +0200

    show header for scrollY less 45px

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,9 +28,10 @@ function classNames(...classes: String[]): string {
 export default function Navbar() {
 	const [show, setShow] = useState(false);
 	const [lastScrollY, setLastScrollY] = useState(0);
+	console.log(typeof window !== 'undefined' && window.scrollY)
 	const controlNavbar = () => {
 		if (typeof window !== 'undefined') {
-			if (window.scrollY < lastScrollY) { // if scroll down hide the navbar
+			if (window.scrollY < 45 || window.scrollY < lastScrollY) { // if scroll down hide the navbar
 				setShow(false);
 			} else { // if scroll up show the navbar
 				setShow(true);
