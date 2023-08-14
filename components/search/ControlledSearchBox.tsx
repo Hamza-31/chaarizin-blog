5422ffe987878ab475e99e678f09601ef1d83413
Hamza <hamza.labzioui@gmail.com>
2023-08-14 18:40:06 +0200

    Remove search icon in the search form

--- components/search/ControlledSearchBox.tsx
+++ components/search/ControlledSearchBox.tsx
@@ -59,7 +59,7 @@ const ControlledSearchBox = ({
 					value={value}
 					name='query'
 					className="opacity-70 min-[400px]:w-[300px] w-full hover:opacity-100 focus:outline-none focus:opacity-100 transition ease-in-out duration-200 bg-beige text-dark-purple block p-4 text-sm" placeholder="Moisture, Oil ..." required />
-				<MagnifyingGlassIcon className="h-6 w-6 text-gray-500 absolute right-2 top-[14px] bottom-[11px] flex items-center pointer-events-none" />
+				{/* <MagnifyingGlassIcon className="h-6 w-6 text-gray-500 absolute right-2 top-[14px] bottom-[11px] flex items-center pointer-events-none" /> */}
 				{/* <button type="submit" className="absolute p-2 right-2.5 bottom-1.5 text-beige rounded bg-dark-purple">Search</button> */}
 
 			</label>
