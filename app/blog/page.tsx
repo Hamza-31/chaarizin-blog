582e9ff7f34bf561ed9a672f35d93af55d33bc66
Hamza <hamza.labzioui@gmail.com>
2023-08-14 18:58:17 +0200

    Fix blog page header padding

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,7 @@ import ArticlesSearch from '@/components/search/ArticlesSearch';
 const BlogPage = () => {
 	return (
 		<>
-			<section className=" relative my-5 flex flex-col justify-center items-center h-32 min-[1024px]:h-40 items-center">
+			<section className=" relative max-[1024px]:my-5 flex flex-col justify-center items-center min-[1024px]:h-40 items-center">
 				<h1 className="uppercase lg:text-6xl md:text-5xl text-4xl">
 					Blog
 				</h1>
